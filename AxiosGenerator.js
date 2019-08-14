efa03d7085df7bc4fc69d0a2d7a0ff1c6fa4f155
anurag-inias <anurag.inias@gmail.com>
2019-08-15 00:01:34 +0530

    First commit

---
+++ AxiosGenerator.js
@@ -0,0 +1,43 @@
+const AxiosGenerator = function () {
+
+    this.generate = function (context, requests, options) {
+        const request = requests[0];
+        const config = {
+            method: request.method,
+            url: request.urlBase,
+            ...extract(request, 'urlParameters', 'params'),
+            ...extract(request, 'headers'),
+            ...extract(request, 'httpBasicAuth', 'auth'),
+            ...extract(request, 'timeout'),
+            ...body(request)
+        };
+
+        return `axios(${JSON.stringify(config, 2, '\t')})`;
+    }
+}
+
+const isEmpty = (value) => {
+    return value === undefined || value === null ||
+        (typeof value === "object" && Object.keys(value).length === 0) ||
+        (typeof value === "string" && value.trim().length === 0)
+}
+
+const extract = (request, pawKey, axiosKey = pawKey) => {
+    if (request[pawKey] && !isEmpty(request[pawKey])) {
+        return { [axiosKey]: request[pawKey] };
+    } else {
+        return {};
+    }
+}
+
+const body = (request) => {
+    if (['PUT', 'POST', 'PATCH'].indexOf(request.method) >= 0) {
+        return { data: request.jsonBody || request.body || {} };
+    } else {
+        return {};
+    }
+}
+
+AxiosGenerator.identifier = "dev.anuragsaini.AxiosGenerator";
+AxiosGenerator.title = "Axios Code Generator";
+registerCodeGenerator(AxiosGenerator);
---
+++ README.md
@@ -0,0 +1,5 @@
+## About
+A [Paw Extension](https://paw.cloud/) for generating [axios](https://github.com/axios/axios) requests.
+
+## License
+This Paw Extension is released under the MIT License. Feel free to fork, and modify!
